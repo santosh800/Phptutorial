d027ba799b56bb12fdb0018cc5b45cd1ac0c6760
khadka santosh <santoshkhadka423@gmail.com>
2016-09-12 18:57:51 +0545

    Add files via upload

---
+++ embending_php_in_html.php
@@ -0,0 +1,17 @@
+<?php
+
+$embend_text="enter your name here...";
+
+?>
+<!doctype html>
+<html>
+<head>
+	<title>embending php in html document</title>
+</head>
+<body>
+	<label for="name">UserName:</label>
+	<!--the value of variable $embend_text is passed to value attribute-->
+	<input type="text" name="name" value="<?php echo $embend_text;?>"/>
+
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+class person_info{
+public  $first_name="ayush";
+public    $second_name="khadka";
+public static $age=24;
+public function get_first_name(){
+/*
+self is for use in static member functions to allow you to access static member variables. 
+$this is used in non-static member functions, 
+and is a reference to the instance of the class on which the member
+ function was called. Because this is an object, you use it like: $this->member
+here,get_first_name() function is non-static member function
+*/
+	return $this->first_name;
+
+}
+ public function get_second_name(){
+return $this->second_name;
+
+
+}
+public static function get_age(){
+return self::$age;
+}
+}
+$person1=new person_info();
+echo $person1->get_first_name();
+echo "<br/>";
+ echo $person1->get_second_name();
+ echo "<br/>";
+ echo person_info::get_age();
+//echo person_info::$first_name." ".person_info::$second_name."<br/>";
+
+?>
